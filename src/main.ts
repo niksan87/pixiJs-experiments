0b445374217b1f17ee5d114283ec71cb2cff2483
Nikolai Stefanov <n_stefanov@hotmail.com>
2020-01-31 18:20:58 +0200

    [ADD] 3 different resize approaches [ADD] Info for each approach [ADD] Sprite

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
+PIXI.WebGLRenderer = PIXI.Renderer;
+window.__PIXI_INSPECTOR_GLOBAL_HOOK__ &&
+    window.__PIXI_INSPECTOR_GLOBAL_HOOK__.register( { PIXI: PIXI } );
+
 window.onload = () => {
     setStyles();
     initApp1( canvasSettings );
     initApp2( canvasSettings );
+    initApp3( canvasSettings );
 };
 
 const canvasSettings = {
-    width: 400,
-    height: 408,
-    backgroundColor: 0xF1EBE9
+    width: 200,
+    height: 204,
+    backgroundColor: 0xF1EBE9,
+    antialias: false
 };
 
 function setStyles() {
@@ -20,8 +26,8 @@ function setStyles() {
         }
         canvas {
             margin: 1px;
-            width: ${window.innerWidth / 2 - 2}px;
-            height: ${window.innerHeight - 8}px;
+            width: ${400}px;
+            height: ${408}px;
         }
     ` ) );
     document.getElementsByTagName( 'head' )[0].appendChild( style );
@@ -31,22 +37,87 @@ function initApp1( settings: object ) {
     const app = window.app1 = new PIXI.Application( settings );
     document.body.appendChild( app.view );
     app.renderer.resize( app.view.clientWidth, app.view.clientHeight );
-    addText( app.stage );
+    const textValue = `Canvas size: 200x208
+Style size: 400x408
+x: 100px
+y: 50px
+fontSize: 20
+scale: 1
+
+renderer.resize() used`;
+    const text = addText( app.stage, textValue );
+    text.x = 20;
+    text.y = 10;
+
+    const img = addSprite( app.stage );
+    img.x = 20;
+    img.y = 200;
 }
 
 function initApp2( settings: object ) {
     const app = window.app2 = new PIXI.Application( settings );
     document.body.appendChild( app.view );
     const coef = app.view.width / app.view.clientWidth;
-    app.stage.scale.set( coef );
-    addText( app.stage );
+    const newFontSize = Math.round( 20 * coef );
+    //app.stage.scale.set( coef );
+    const textValue = `Canvas size: 200x208
+Style size: 400x408
+x: 100px
+y: 50px
+fontSize: ${newFontSize}
+scale: ${coef}`;
+    const text = addText( app.stage, textValue, newFontSize );
+    text.x = 20;
+    text.y = 10;
+
+    const img = addSprite( app.stage );
+    setTimeout( () => {
+        img.width *= coef;
+        img.height *= coef;
+        img.x = 20;
+        img.y = 200 * coef;
+    }, 500 );
+
+
 }
 
-function addText( app: PIXI.Container ) {
+function initApp3( settings: object ) {
+    const app = window.app1 = new PIXI.Application( settings );
+    document.body.appendChild( app.view );
+    const coef = app.view.width / app.view.clientWidth;
+    app.stage.scale.set( coef );
+    const textValue = `Canvas size: 200x208
+Style size: 400x408
+x: 100px
+y: 50px
+fontSize: 20
+scale: ${coef}
+
+rootContainer scaled`;
+    const text = addText( app.stage, textValue );
+    text.x = 20;
+    text.y = 10;
+
+    const img = addSprite( app.stage );
+    img.x = 20;
+    img.y = 200;
+}
+
+function addText( app: PIXI.Container, textValue, fontSize = 20 ) {
     const style = new PIXI.TextStyle( {
         fontFamily: 'Times new roman',
-        fontSize: 30
+        fontSize: fontSize
     } );
-    const text = new PIXI.Text( 'A test text here.', style );
+    const text = new PIXI.Text( textValue, style );
+    text.fontSize = fontSize;
     app.addChild( text );
+    return text;
 }
+
+function addSprite( app: PIXI.Container ) {
+    const img = new PIXI.Sprite( PIXI.Texture.from( '../src/img.png' ) );
+    app.addChild( img );
+    return img;
+}
+
+
